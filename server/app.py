cd66a7ffcb73b10459b3e2afecc7b70d1be60f1c
sri-nivas1227 <srinivasmekala1227@gmail.com>
2025-08-13 20:11:49 -0400

    fixed bugs in auth endpoints and registered auth apis

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from UrlEndpoint import urlRouter
 from CategoryEndpoint import categoryRouter
+from AuthEndpoints import auth_router
 from flask_cors import CORS
 
 # Load environment variables
@@ -11,6 +12,7 @@ load_dotenv()
 # Initialize Flask app
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:3000"])
+app.register_blueprint(auth_router)
 app.register_blueprint(urlRouter)
 app.register_blueprint(categoryRouter)
 
