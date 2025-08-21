d195c0067ed8ac6bb99567c1cdc9763ffd325512
sri-nivas1227 <srinivasmekala1227@gmail.com>
2025-08-21 15:28:36 -0400

    redirect to home on root

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,12 +1,8 @@
-import Hero from "./components/Hero";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  redirect("/home");
   return (
-    <div className="font-mono flex flex-col justify-between items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <Hero />
-      <div className="">
-        <p className="cursor-pointer text-xl">Show all links</p>
-      </div>
-    </div>
+    <div className="font-mono flex flex-col justify-between items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"></div>
   );
 }
