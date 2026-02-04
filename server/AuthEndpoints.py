a652d4f37e89ceea11a195588fe0a1de478780d1
sri-nivas1227 <srinivasmekala1227@gmail.com>
2026-02-04 18:23:29 -0500

    ✨ Add JWT authentication to login endpoint and update MongoDB URI variable

--- server/AuthEndpoints.py
+++ server/AuthEndpoints.py
@@ -1,6 +1,11 @@
-from flask import request, Blueprint
+from flask import request, Blueprint, make_response
 from db import db
 from bcrypt import hashpw, gensalt, checkpw
+import jwt
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 auth_router = Blueprint('auth', __name__)
 userCollection = db.get_collection('users')
 
@@ -32,5 +37,11 @@ def login():
     user = userCollection.find_one({"email": email})
 
     if user and checkpw(password.encode('utf-8'), user['password'].encode('utf-8')):
-        return {"message": "Login successful", "user_id": str(user['_id'])}, 200
+        jwt_secret = os.getenv("JWT_SECRET")
+        jwt_token = jwt.encode({"user_id":str(user["_id"]), "full_name":user["full_name"]}, jwt_secret)
+        # write jwt token to the cookies
+
+        response = make_response({"message": "Login successful", "user_id": str(user['_id']), "token": jwt_token}, 200)
+        # response.set_cookie('token', jwt_token)
+        return response
     return {"message": "Invalid email or password"}, 401
--- server/db.py
+++ server/db.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
-mongo = MongoClient(os.getenv("MONGO_URI", "mongodb://localhost:27017/linkhub"))
+mongo = MongoClient(os.getenv("MONGODB_URI", "mongodb://localhost:27017/linkhub"))
 
 db = mongo.get_database("linkhub")
 
