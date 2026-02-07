de93c51d1bc7ed8c772362a84f031c20f49ad1c9
sri-nivas1227 <srinivasmekala1227@gmail.com>
2026-02-07 01:25:47 -0500

    ✨ Refactor get_categories endpoint to validate token and improve error handling; rename method for fetching categories by user ID

--- server/CategoryEndpoint.py
+++ server/CategoryEndpoint.py
@@ -1,15 +1,28 @@
 from flask import Blueprint, jsonify, request
 from bson import ObjectId
 from models.Category import Category
+from helpers.utilities import validate_and_get_token_payload
 
 # Create a Blueprint for Category endpoints
 categoryRouter = Blueprint('category', __name__)
 
 @categoryRouter.route('/categories', methods=['GET'])
 def get_categories():
-    user_id = request.args.get('user_id')
-    categories = Category.get_name_id_by_user_id(user_id) if user_id else Category.get_all()
+    # get token from cookies
+    token = request.cookies.get('token')
+    is_valid_token, payload = validate_and_get_token_payload(token) if token else False
+    if is_valid_token:
+        user_id = payload.get('user_id')
+    else:
+        return jsonify({
+            "success": False,
+            "message": "Invalid or missing token"
+        }), 401
+    print(f"User ID from token: {user_id}")
+    categories = Category.get_categories_by_user_id(user_id) if user_id else Category.get_all()
+    print(f"Categories fetched: {categories}")
     return jsonify({
+        "success": True,
         "message": "List of categories",
         "data": categories
     }), 200
