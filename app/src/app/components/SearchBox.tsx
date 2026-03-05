7edca537b2a653d2477e27df23db7ba7fc12824e
sri-nivas1227 <srinivasmekala1227@gmail.com>
2026-03-05 17:27:28 -0500

    ✨ Add SearchBox component: implement search input with debounced query handling

---
+++ app/src/app/components/SearchBox.tsx
@@ -0,0 +1,32 @@
+import { SearchIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+
+interface SearchBoxProps {
+  setSearchQuery: (query: string) => void;
+}
+export default function SearchBox({
+  setSearchQuery,
+}: SearchBoxProps) {
+  const [searchInput, setSearchInput] = useState("");
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setSearchQuery(searchInput);
+    }, 500);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [searchInput]);
+  return (
+    <div className="flex items-center px-2 outline-none border rounded-2xl border-zinc-700 focus-within:border-indigo-500 transition-colors">
+      <input
+        type="text"
+        placeholder="Search..."
+        className="outline-none w-full p-2"
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
+      />
+      <SearchIcon size={18} />
+    </div>
+  );
+}
